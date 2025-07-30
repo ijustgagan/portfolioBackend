6e00f42fe359c04c42c9da096c9ce4b01df3794d
ijustgagan <rathoregagan726@gmail.com>
2025-07-30 15:10:02 +0530

    changed

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import path from "path";
+// import path from "path";
+import cors from "cors";
 export const app = express();
+
+
+app.use(cors({
+  origin: "*",
+  credentials: false,
+}));
+
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -10,8 +18,9 @@ app.use(cookieParser());
 import { userRouter } from "./routes/User.js";
 app.use("/api/v1", userRouter);
 
-app.use(express.static(path.resolve("./frontend/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve("./frontend/build/index.html"));
-});
+// app.use(express.static(path.resolve("./frontend/build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve("./frontend/build/index.html"));
+// });
