5b40f12fe61df66576b67035f7c1411690486c22
ijustgagan <rathoregagan726@gmail.com>
2025-07-30 16:44:46 +0530

    added mongoose use case

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+mongoose.set('strictQuery', true); 
 dotenv.config({ path: "./config/.env" });
 
 const Mongo_Url = process.env.MONGO_URL;
-
 export const connectDatabase = () => {
-  mongoose.connect(Mongo_Url, { useNewUrlParser: true, useUnifiedTopology: true })
+  mongoose.connect(Mongo_Url,)
     .then((c) => {
       console.log(`MongoDB connected to: ${c.connection.host}`);
     })
